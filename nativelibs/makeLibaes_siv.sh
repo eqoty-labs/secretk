4adc4368c856a5f737c080d70f37057b58c39c20
Luca Spinazzola <mspinluca@gmail.com>
2022-04-20 22:40:37 -0400

    add script to make libaes_siv for all apple devices. add ios-cmake and OpenSSL (for apple devices) submodules to nativelibs
    https://github.com/leetal/ios-cmake
    https://github.com/krzyzanowskim/OpenSSL

---
+++ nativelibs/makeLibaes_siv.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# darwin target names come from here: https://github.com/leetal/ios-cmake
+targets="OS64 SIMULATOR64 SIMULATORARM64 MAC MAC_ARM64"
+
+
+for target in ${targets}
+do    
+    # create one build dir per target architecture
+    mkdir -p ./libaes_siv_build/${target}
+    cd ./libaes_siv_build/${target}
+
+    case $target in
+
+      "OS64"|"OS64COMBINED"|"SIMULATOR64"|"SIMULATORARM64"|"TVOS"|"TVOSCOMBINED"|"SIMULATOR_TVOS"|"WATCHOS"| \
+      "WATCHOSCOMBINED"|"SIMULATOR_WATCHOS"|"MAC"|"MAC_ARM64"|"MAC_CATALYST"|"MAC_CATALYST_ARM64")
+        OPENSSL_ROOT_DIR=../../darwinopenssl/iphoneos
+        OPENSSL_INCLUDE_DIR=../../darwinopenssl/iphoneos/include
+        OPENSSL_CRYPTO_LIBRARY=../../darwinopenssl/iphoneos/lib/libcrypto.a
+        cmake . -S../../libaes_siv \
+          -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
+          -DOPENSSL_CRYPTO_LIBRARY=$OPENSSL_CRYPTO_LIBRARY \
+          -DOPENSSL_INCLUDE_DIR=$OPENSSL_INCLUDE_DIR \
+          -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64
+        cmake --build . --target aes_siv_static --config Release \
+          -- -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
+          -DOPENSSL_CRYPTO_LIBRARY=$OPENSSL_CRYPTO_LIBRARY \
+          -DOPENSSL_INCLUDE_DIR=$OPENSSL_INCLUDE_DIR
+      ;;
+      *)
+        cmake . -S../../libaes_siv
+        make -j32
+      ;;
+    esac
+    
+    #Add -DIOS_PLATFORM=SIMULATOR if you intent to run it there
+    cd -
+done
+
