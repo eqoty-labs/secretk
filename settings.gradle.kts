bb3116133ab926a6d1c84ac0d8903e6101d8e6aa
Luca Spinazzola <mspinluca@gmail.com>
2022-04-29 23:33:28 -0400

    us my own package readonly personal access token for khash
    Repository wasn't

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ dependencyResolutionManagement {
             credentials {
                 username = "token"
                 // see: https://github.community/t/download-from-github-package-registry-without-authentication/14407/44
-                password = "\u0039\u0032\u0037\u0034\u0031\u0064\u0038\u0033\u0064\u0036\u0039\u0061\u0063\u0061\u0066\u0031\u0062\u0034\u0061\u0030\u0034\u0035\u0033\u0061\u0063\u0032\u0036\u0038\u0036\u0062\u0036\u0032\u0035\u0065\u0034\u0061\u0065\u0034\u0032\u0062"
+                password = "\u0067\u0068\u0070\u005f\u0072\u0067\u0070\u0054\u0069\u006c\u004c\u0042\u0038\u006c\u0066\u0057\u0045\u0056\u0031\u0053\u004a\u006d\u0061\u006b\u004a\u004c\u005a\u0063\u0038\u004d\u0074\u0038\u0045\u0066\u0031\u0059\u0053\u0058\u0039\u0055"
             }
         }
     }
