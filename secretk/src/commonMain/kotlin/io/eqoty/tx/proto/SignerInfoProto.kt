3fd91e85148dff53450235b5301d1579688d6504
Luca Spinazzola <mspinluca@gmail.com>
2022-09-21 16:44:18 -0400

    fix proto reference url

--- secretk/src/commonMain/kotlin/io/eqoty/tx/proto/SignerInfoProto.kt
+++ secretk/src/commonMain/kotlin/io/eqoty/tx/proto/SignerInfoProto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.protobuf.ProtoNumber
 
 /***
  * Reference:
- * https://github.com/scrtlabs/secret.js/blob/master/src/protobuf_stuff/cosmos/tx/v1beta1/tx.ts
+ * https://github.com/scrtlabs/SecretNetwork/blob/master/cosmwasm/enclaves/execute/proto/cosmos/tx/v1beta1/tx.proto
  */
 @kotlinx.serialization.Serializable
 class SignerInfoProto(
