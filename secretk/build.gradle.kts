73d280f8c3bee63f5ec517aee45a32635306dda3
Luca Spinazzola <mspinluca@gmail.com>
2022-09-28 20:23:49 -0400

    update secretjs

--- secretk/build.gradle.kts
+++ secretk/build.gradle.kts
@@ -120,7 +120,7 @@ kotlin {
                 implementation(npm("os-browserify", "^0.3.0"))
                 implementation(npm("miscreant", "^0.3.2"))
                 implementation(npm("libsodium-wrappers-sumo", "^0.7.10"))
-                implementation(npm("secretjs", "^1.4.0-alpha.10"))
+                implementation(npm("secretjs", "^1.4.1"))
                 implementation(npm("google-protobuf", "^3.21.0"))
                 implementation(npm("@peculiar/webcrypto", "^1.4.0"))
                 implementation(npm("@happy-dom/global-registrator", "^6.0.4"))
