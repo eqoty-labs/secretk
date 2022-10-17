2bf65fbb644ed5f66d66555d2e217069d05922e1
Luca Spinazzola <mspinluca@gmail.com>
2022-10-17 15:25:52 -0400

    add BigInteger coin constructor and implement +,-,* operators for Coin

--- secretk/src/commonMain/kotlin/io/eqoty/secretk/types/Coin.kt
+++ secretk/src/commonMain/kotlin/io/eqoty/secretk/types/Coin.kt
@@ -1,5 +1,6 @@
 package io.eqoty.secretk.types
 
+import com.ionspin.kotlin.bignum.integer.BigInteger
 import io.eqoty.secretk.types.proto.CoinProto
 
 @kotlinx.serialization.Serializable
@@ -8,6 +9,47 @@ data class Coin(
     val amount: String
 ) {
     constructor(denom: String, amount: Int) : this(denom, amount.toString())
+    constructor(denom: String, amount: BigInteger) : this(denom, amount.toString())
 
     fun toProto() = CoinProto(denom, amount)
+
+    operator fun plus(increment: Coin): Coin {
+        return if (increment.denom == denom) {
+            this.copy(
+                amount = (BigInteger.parseString(amount, 10) + BigInteger.parseString(
+                    increment.amount,
+                    10
+                )).toString()
+            )
+        } else {
+            this
+        }
+    }
+
+    operator fun minus(increment: Coin): Coin {
+        return if (increment.denom == denom) {
+            this.copy(
+                amount = (BigInteger.parseString(amount, 10) - BigInteger.parseString(
+                    increment.amount,
+                    10
+                )).toString()
+            )
+        } else {
+            this
+        }
+    }
+
+    operator fun times(increment: Coin): Coin {
+        return if (increment.denom == denom) {
+            this.copy(
+                amount = (BigInteger.parseString(amount, 10) * BigInteger.parseString(
+                    increment.amount,
+                    10
+                )).toString()
+            )
+        } else {
+            this
+        }
+    }
+
 }
