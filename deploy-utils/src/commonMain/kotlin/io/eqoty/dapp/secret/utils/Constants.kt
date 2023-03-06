b9929d3bc27549f5e13e04f6dcfa9b58e81d0bc9
Luca Spinazzola <mspinluca@gmail.com>
2023-03-05 23:47:19 -0500

    Remove eqoty releated paths from deploy-utils constants

--- deploy-utils/src/commonMain/kotlin/io/eqoty/dapp/secret/utils/Constants.kt
+++ deploy-utils/src/commonMain/kotlin/io/eqoty/dapp/secret/utils/Constants.kt
@@ -1,15 +1,7 @@
 package io.eqoty.dapp.secret.utils
 
-import io.getenv
-import okio.Path
-import okio.Path.Companion.toPath
-
 object Constants {
     const val NODE_TYPE_ENV_NAME = "NODE_TYPE"
     const val GITPOD_ID_ENV_NAME = "GITPOD_ID"
     const val CONTRACT_PATH_ENV_NAME = "CONTRACT_PATH"
-
-    val releaseContractCodePath: Path = "${getenv(CONTRACT_PATH_ENV_NAME)}/eqoty_release.wasm.gz".toPath()
-    val royaltyContractCodePath: Path = "${getenv(CONTRACT_PATH_ENV_NAME)}/eqoty_royalty.wasm.gz".toPath()
-    val purchaseContractCodePath: Path = "${getenv(CONTRACT_PATH_ENV_NAME)}/eqoty_purchase.wasm.gz".toPath()
 }
