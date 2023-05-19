e91bc351964c4f3918c35ca62f0cea795a5a1f19
Luca Spinazzola <mspinluca@gmail.com>
2023-05-19 15:59:09 -0400

    migrate to official KHash release. closes #3

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -88,7 +88,7 @@ kotlin {
                 implementation(libs.bignum.serialization.kotlinx)
                 implementation(libs.com.ionspin.kotlin.multiplatformCryptoLibsodiumBindings)
                 implementation(libs.io.github.luca992.cash.z.ecc.android.kotlinBip39)
-                implementation(libs.com.github.luca992.khash.ripemd160)
+                implementation(libs.com.github.komputing.khash.ripemd160)
                 implementation(libs.io.eqoty.kryptools.aessiv)
                 implementation(libs.io.eqoty.kryptools.bech32)
                 implementation(libs.io.eqoty.kryptools.axlsign)
