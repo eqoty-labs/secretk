c987565af1561c1629ed6d297db15794523e5475
Luca Spinazzola <mspinluca@gmail.com>
2024-02-19 03:08:54 -0500

    update to kotlin 1.9.22 & all dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
     }
     plugins {
         // See https://jmfayard.github.io/refreshVersions
-        id("de.fayard.refreshVersions") version "0.60.3"
+        id("de.fayard.refreshVersions") version "0.60.5"
     }
 }
 
